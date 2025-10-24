c4ee81ccf21995122e1893c224b5d2499c523860
cassio <bindaco77@gmail.com>
2025-10-24 15:54:05 -0300

    .

--- Program.cs
+++ Program.cs
@@ -1,13 +1,1 @@
 ﻿
-using App.Models;
-using App.Repositories;
-
-var carregar = JsonRepositorio.LerArquivo();
-
-var teste = new Filme("Filme de teste");
-
-carregar.Add(teste);
-
-// var teste = carregar.FirstOrDefault(x => x.Titulo == "Filme de teste");
-
-Console.WriteLine(JsonRepositorio.SalvarArquivo(carregar));
